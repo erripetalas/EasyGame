28a35124416b618103a574c74f82d22766b90bec
Erri <erri_perdana@hotmail.com>
2025-09-11 21:30:58 +0930

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EasyGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
